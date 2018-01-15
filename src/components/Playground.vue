c7bfdb03e477e942bf708e9acbdf3d92c0f8d4c7
Brandon Deo <bdeo@udel.edu>
2018-01-14 20:58:20 -0500

    Remove unncessary boilerplate code

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -9,29 +9,6 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  name: 'Playground',
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
